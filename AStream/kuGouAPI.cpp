aa56ba10e949ba4c992c3a6b829f009e68d934d5
Amber-yy <1397011447@qq.com>
2016-11-13 21:24:49 +0800

    添加部分注释

--- AStream/kuGouAPI.cpp
+++ AStream/kuGouAPI.cpp
@@ -30,9 +30,9 @@ QString kuGouAPI::playLinkHandle(QNetworkReply *reply)
 	QByteArray json = reply->readAll();
 	QJsonParseError error;
 	QJsonDocument doucoument = QJsonDocument::fromJson(json, &error);
-	QJsonObject obj = doucoument.object();
+	QJsonObject obj = doucoument.object();//响应播放请求
 
-	return obj.take("url").toString();
+	return obj.take("url").toString();//解析播放链接JSON
 }
 
 QVector<musicAPI::musicInfo> kuGouAPI::infoHandle(QNetworkReply *reply)
@@ -44,28 +44,28 @@ QVector<musicAPI::musicInfo> kuGouAPI::infoHandle(QNetworkReply *reply)
 
 	QByteArray json = reply->readAll();
 	int size;
-	json.chop(1);
-	json.remove(0, 17);
+	json.chop(1);//从尾部删除一个字符
+	json.remove(0, 17);//从头部删除一段字符
 	QJsonParseError error;
 	QJsonDocument doucoument = QJsonDocument::fromJson(json, &error);
-	QJsonObject obj = doucoument.object();
+	QJsonObject obj = doucoument.object();//字符串转换为JSON对象
 
-	QJsonArray jsArray = obj.take("data").toObject().take("info").toArray();
-	size = jsArray.size();
+	QJsonArray jsArray = obj.take("data").toObject().take("info").toArray();//data里是有用的数据
+	size = jsArray.size();//数据条数
 
 	QVector<musicInfo> t(size);
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)//遍历整个JSON
 	{
-		t[i].hashCode = jsArray[i].toObject().take("hash").toString();
+		t[i].hashCode = jsArray[i].toObject().take("hash").toString();//取出其中键为hash的值
 		if (t[i].hashCode == "")
 		{
 			t[i].hashCode = jsArray[i].toObject().take("320hash").toString();
 		}
 		t[i].musicName = jsArray[i].toObject().take("songname").toString();
 		t[i].singer = jsArray[i].toObject().take("singername").toString();
-		t[i].fileName = t[i].singer + "-" + t[i].musicName;
-		t[i].duration = jsArray[i].toObject().take("duration").toInt();
+		t[i].fileName = t[i].singer + "-" + t[i].musicName;//将歌手名和音乐名拼接成文件名
+		t[i].duration = jsArray[i].toObject().take("duration").toInt();//获取时长
 	}
 
 	return t;
